4fdb32ef105932587a72815582785b44bfc38198
Matt Lacey <matt@mrlacey.co.uk>
2020-04-17 22:12:08 +0100

    add appropriate suppressions

---
+++ src/ConstVisualizer/GlobalSuppressions.cs
@@ -0,0 +1,9 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1611:Element parameters should be documented", Justification = "<Pending>", Scope = "member", Target = "~M:ConstVisualizer.StringExtensions.IsValidVariableNameAsync(System.String,System.Char,System.Char)~System.Threading.Tasks.Task{System.Boolean}")]
+[assembly: SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1615:Element return value should be documented", Justification = "<Pending>", Scope = "member", Target = "~M:ConstVisualizer.StringExtensions.IsValidVariableNameAsync(System.String,System.Char,System.Char)~System.Threading.Tasks.Task{System.Boolean}")]
