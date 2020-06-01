f5d83502b09e465d5172256b1b66104ee1d0654b
Matt Lacey <matt@mrlacey.co.uk>
2020-06-01 14:33:37 +0100

    version number to 1.5

--- src/ConstVisualizer/GlobalSuppressions.cs
+++ src/ConstVisualizer/GlobalSuppressions.cs
@@ -1,8 +1,11 @@
-﻿// This file is used by Code Analysis to maintain SuppressMessage
+﻿// <copyright file="GlobalSuppressions.cs" company="Matt Lacey">
+// Copyright (c) Matt Lacey. All rights reserved.
+// </copyright>
+
+// This file is used by Code Analysis to maintain SuppressMessage
 // attributes that are applied to this project.
 // Project-level suppressions either have no target or are given
 // a specific target and scoped to a namespace, type, member, etc.
-
 using System.Diagnostics.CodeAnalysis;
 
 [assembly: SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1611:Element parameters should be documented", Justification = "No value in documenting this.")]
