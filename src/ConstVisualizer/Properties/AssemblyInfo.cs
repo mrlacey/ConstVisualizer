d3e91f80fdfc3066fc5b6ca2d24dd581962eeee7
Hazel Megan <matt@mrlacey.co.uk>
2021-12-10 20:33:17 +0000

    v1.11 - VS2019 specific version

--- src/ConstVisualizer/Properties/AssemblyInfo.cs
+++ src/ConstVisualizer/Properties/AssemblyInfo.cs
@@ -14,5 +14,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("1.10.*")]
-[assembly: AssemblyFileVersion("1.10.0.0")]
+[assembly: AssemblyVersion("1.11.*")]
+[assembly: AssemblyFileVersion("1.11.0.0")]
