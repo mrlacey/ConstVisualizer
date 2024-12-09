a28008881822d28a8f832c7531c7ca8d80b8d54e
Matt Lacey <matt@mrlacey.co.uk>
2024-12-09 08:23:09 +0000

    Add additional analytics data

--- src/ConstVisualizer/ConstVisualizerPackage.cs
+++ src/ConstVisualizer/ConstVisualizerPackage.cs
@@ -86,6 +86,8 @@ namespace ConstVisualizer
 				{
 					{ "VsixVersion", Vsix.Version },
 					{ "VsVersion", Microsoft.VisualStudio.Telemetry.TelemetryService.DefaultSession?.GetSharedProperty("VS.Core.ExeVersion") },
+					{ "Architecture", RuntimeInformation.ProcessArchitecture.ToString() },
+					{ "MsInternal", Microsoft.VisualStudio.Telemetry.TelemetryService.DefaultSession?.IsUserMicrosoftInternal.ToString() },
 				};
 
 				client.TrackEvent(Vsix.Name, properties);
